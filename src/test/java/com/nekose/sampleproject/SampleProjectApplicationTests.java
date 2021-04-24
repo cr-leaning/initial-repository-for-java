469c3b48b6e80bf7b968c81780baa19db9e09906
yuki-suzuki <yukisuzuki8105@gmail.com>
2021-04-24 10:16:59 +0900

    add some ut

--- src/test/java/com/nekose/sampleproject/SampleProjectApplicationTests.java
+++ src/test/java/com/nekose/sampleproject/SampleProjectApplicationTests.java
@@ -3,16 +3,10 @@ package com.nekose.sampleproject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.function.Function;
-
 @SpringBootTest
 class SampleProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
-	}
-
-	public void test() {
-		Function<String, Integer> f = s -> Integer.parseInt(s);
 	}
 }
