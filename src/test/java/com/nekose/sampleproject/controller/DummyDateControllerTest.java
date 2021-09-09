5de97128cec41357cd3df97b9892588a0e2e6ccc
yuki suzuki <64686566+yuki-suzu@users.noreply.github.com>
2021-09-10 08:48:25 +0900

    add some ut (#11)

--- src/test/java/com/nekose/sampleproject/controller/DummyDateControllerTest.java
+++ src/test/java/com/nekose/sampleproject/controller/DummyDateControllerTest.java
@@ -6,6 +6,7 @@ import com.nekose.sampleproject.domain.model.entity.DummyData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -13,6 +14,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -64,7 +66,10 @@ class DummyDateControllerTest {
                                 "}}")
                 ).andReturn();
 
-        verify(dummyApplication, times(1)).get(exceptedKey);
+        var captor = ArgumentCaptor.forClass(String.class);
+        verify(dummyApplication, times(1)).get(captor.capture());
+
+        assertEquals(exceptedKey, captor.getValue());
     }
 
 }
--- src/test/java/com/nekose/sampleproject/infrastracture/rdb/SampleDataRepositoryTest.java
+++ src/test/java/com/nekose/sampleproject/infrastracture/rdb/SampleDataRepositoryTest.java
@@ -4,6 +4,8 @@ import com.nekose.sampleproject.application.repository.SampleDataRepository;
 import com.nekose.sampleproject.config.DatasourceConfig;
 import com.nekose.sampleproject.config.property.SampleProperties;
 import com.nekose.sampleproject.infrastructure.rdb.SampleTableRepositoryImpl;
+import com.nekose.sampleproject.infrastructure.rdb.entity.SampleTableEntity;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -15,6 +17,10 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ActiveProfiles({"local", "test"})
 @ExtendWith(SpringExtension.class)
@@ -29,7 +35,30 @@ import org.springframework.test.context.support.DependencyInjectionTestExecution
         DependencyInjectionTestExecutionListener.class
 })
 @EnableConfigurationProperties
-public class SampleDataRepositoryTest {
+class SampleDataRepositoryTest {
     @Autowired
     private SampleDataRepository sampleDataRepository;
+
+    @Test
+    void storeAndGet() {
+        var expected = SampleTableEntity.builder()
+                .name("testName")
+                .value("testValue")
+                .build();
+
+        var id = sampleDataRepository.store(expected);
+        var actual = sampleDataRepository.findById(id);
+
+        assertTrue(actual.isPresent());
+        assertAll(
+                () -> {
+                    var actualEntity = actual.get();
+
+                    assertAll(
+                            () -> assertEquals(expected.getName(), actualEntity.getName()),
+                            () -> assertEquals(expected.getValue(), actualEntity.getValue())
+                    );
+                }
+        );
+    }
 }
